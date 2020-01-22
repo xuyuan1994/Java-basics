b8784f2d7418b779ec162b8c4ff45e84a686594b
xuyuan1994 <yuanx5@uci.edu>
2020-01-21 16:59:02 -0800

    second commit

--- Bank_account.java
+++ Bank_account.java
@@ -6,12 +6,13 @@ public class Bank_account{
     String customer_name="Rex";
     String email="yuanx5@gmail.com";
     int phone_number=818263213;
+    Scanner scanner = new Scanner(System.in);
     public void deposit(double amount){
         balance+=amount;
     }
     public void withdrawal(double amount){
         if (amount>balance){
-            System.out.println("Sorry, your lack of funding");
+            System.out.println("Sorry, your account lacks of funding");
         }
         else{
             balance-=amount;
@@ -26,22 +27,48 @@ public class Bank_account{
         System.out.println("The user's phone_number is "+phone_number);
 
     }
-    public void whichcommands(String command ){
+    public void whichcommand(String command, Bank_account account ){
         switch (command){
             case "withdrawal":
             {
+                System.out.println("Please enter the amount that you want to withdraw");
+                Double amonunt=scanner.nextDouble();
+                account.withdrawal(amonunt);
+                break;
+            }
+            case  "deposit":
+            {
+                System.out.println("Please enter the amount that you want to deposit");
+                Double amonunt=scanner.nextDouble();
+                account.deposit(amonunt);
+                break;
+            }
+            case "print user info":
+            {   
+                System.out.println("Here are your info");
+                account.printinfo();
+                break;
 
             }
+            case "exit":
+            {
+                System.out.println("You have exited");
+                break;
+            }
+            default:
+            {
+                System.out.println("Sorry, you did not enter a valid command, googbye");
+                break;
+            }
+
         }
     }
     public static void main (String[] args){
         Bank_account rex_account=new Bank_account();
-        rex_account.printinfo();
-        rex_account.withdrawal(100);
-        rex_account.withdrawal(40);
-        rex_account.deposit(100);
-        System.out.println("after some deposits and withdrawals");
-        rex_account.printinfo();
+        System.out.println("please enter one of the following commands: withdrawal, deposit, print user info or exit " );
+        Scanner scanner2 = new Scanner(System.in);
+        String command =scanner2.next();
+        rex_account.whichcommand(command, rex_account);
 
     }
 };
